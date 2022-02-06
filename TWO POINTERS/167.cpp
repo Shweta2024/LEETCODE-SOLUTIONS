dec683860924bffbc5441cac3fc8e1c6b255a466
Shweta Bhagat <bhagatshweta0216@gmail.com>
2022-02-07 03:46:15 +0530

    Two Sum II - Input Array Is Sorted

---
+++ TWO POINTERS/167.cpp
@@ -0,0 +1,33 @@
+/*
+167. Two Sum II - Input Array Is Sorted
+
+https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+
+*/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target)
+    {
+        int low=0;
+        int high=numbers.size()-1;
+        vector<int>ans;
+        
+        while(low<high)
+        {
+            if(numbers[low]+numbers[high]==target)
+            {
+                ans.push_back(low+1);
+                ans.push_back(high+1);
+                break;
+            }
+            else if(numbers[low]+numbers[high]<target)
+                low++;
+            
+            else
+                high--;
+        }
+        return ans;
+    }
+};
